6990513b7de8ba772d2261fdb4f07cd0b0057524
sander-hergarten <mail@sander-hergarten.de>
2022-09-22 19:03:11 +0200

    DEV: added basic environment tests and fixed bugs

--- src/environments/procgen_envs.py
+++ src/environments/procgen_envs.py
@@ -1,6 +1,7 @@
 # %%
 # Imports
 import gym
+import procgen
 from tf_agents.environments import suite_gym
 from tf_agents.environments import tf_py_environment
 
@@ -8,7 +9,7 @@ from tf_agents.environments import tf_py_environment
 # Helpers
 
 gym_to_tf = lambda env: tf_py_environment.TFPyEnvironment(suite_gym.wrap_env(env))
-load_gym_env = lambda env_name: gym_to_tf(gym.make(f"procgen:procgen-{env_name}-v0"))
+load_gym_env = lambda env_name: gym.make(f"procgen-{env_name}-v0")
 
 # %%
 # List Environemnents
@@ -33,4 +34,10 @@ env_names = [
 
 # %%
 # Load Environments
-envirnment_dictionary = {env_name: load_gym_env(env_name) for env_name in env_names}
+environment_dictionary_gym = {
+    env_name: load_gym_env(env_name) for env_name in env_names
+}
+environment_dictionary_tf = {
+    env_name: gym_to_tf(env) for env_name, env in environment_dictionary_gym.items()
+}
+# %%
