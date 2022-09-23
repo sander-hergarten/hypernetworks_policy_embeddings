329fca6331d7bab215208dfb919734980cefd1a7
sander-hergarten <mail@sander-hergarten.de>
2022-09-23 21:24:18 +0200

    REFACTOR: removed agent from buffer generation

--- src/replay_buffers/generate_buffer.py
+++ src/replay_buffers/generate_buffer.py
@@ -1,67 +1,36 @@
 # Imports
-from abc import ABC
 import tensorflow as tf
 
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
-from tf_agents import specs
-from tf_agents.trajectories import time_step as ts
 from tf_agents.agents.dqn import dqn_agent
 from tf_agents.networks import q_network
 from tf_agents.drivers import dynamic_episode_driver
 
 
-# ABC
-class BufferGenerator(ABC):
-    """this Abstract Base Class defines the interface for all buffer generators"""
+class BufferGenerator:
+    """This Class defines the interface for all buffer generators"""
 
-    agent = NotImplemented
-    num_train_steps: int = NotImplemented
-    is_trained = False
+    buffer = None
 
     def __init__(self, tf_env) -> None:
-        self.tf_env = tf_env
-
-        ...
-
-    def generate_buffer(
-        self, num_episodes: int
-    ) -> tf_uniform_replay_buffer.TFUniformReplayBuffer:
-        """generates a buffer using the DQN agent. If the agent is not trained, it will train it first
+        """initalizes the buffer generator
 
         Args:
-            num_episodes (int): the number of episodes to generate the buffer with
-
-        Returns:
-            tf_uniform_replay_buffer.TFUniformReplayBuffer: the buffer
+            tf_env (tf.Environment): the environment to generate the buffer with
+            agent (tf.Agent): the agent that acts in the environment
         """
+        self.tf_env = tf_env
 
-        if not self.is_trained:
-            self.train()
-
-        self.buffer = self._generate_buffer(num_episodes)
+    def get_last_buffer(self) -> tf_uniform_replay_buffer.TFUniformReplayBuffer | None:
         return self.buffer
 
-    def train(self):
-        """trains the agent for self.num_train_steps. When the training is complete, the agent is marked as trained"""
-        print("training")
-        buffer = self._generate_buffer(self.num_train_steps)
-        dataset = buffer.as_dataset(sample_batch_size=4, num_steps=2)
+    def generate_buffer(
+        self, num_episodes: int, data_spec, policy
+    ) -> tf_uniform_replay_buffer.TFUniformReplayBuffer:
 
-        iterator = iter(dataset)
-
-        for _ in range(self.num_train_steps):
-            trajectories, _ = next(iterator)
-            loss = self.agent.train(experience=trajectories)
-
-        self.is_trained = True
-
-    def get_last_buffer(self) -> tf_uniform_replay_buffer.TFUniformReplayBuffer:
-        return self.buffer
-
-    def _generate_buffer(self, num_episodes):
         print(f"generating buffer {num_episodes} episodes")
         replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
-            self.agent.collect_data_spec,
+            data_spec,
             batch_size=self.tf_env.batch_size,
             max_length=num_episodes,
         )
@@ -70,58 +39,10 @@ class BufferGenerator(ABC):
 
         dynamic_episode_driver.DynamicEpisodeDriver(
             self.tf_env,
-            self.agent.collect_policy,
+            policy,
             observers=replay_observer,
             num_episodes=num_episodes,
         ).run()
 
+        self.buffer = replay_buffer
         return replay_buffer
-
-
-class ProcgenBufferGenerator(BufferGenerator):
-    def __init__(self, tf_env) -> None:
-        super().__init__(tf_env)
-
-        self.observation_spec = specs.TensorSpec(
-            shape=(64, 64, 3), dtype=tf.uint8, name="observation"
-        )
-        self.discount_spec = specs.BoundedTensorSpec(
-            shape=(), dtype=tf.float32, minimum=0, maximum=1, name="discount"
-        )
-        self.reward_spec = specs.TensorSpec(shape=(), dtype=tf.float32, name="reward")
-        self.step_type_spec = specs.TensorSpec(
-            shape=(), dtype=tf.int32, name="step_type"
-        )
-        self.action_spec = specs.BoundedTensorSpec([], tf.int32, minimum=0, maximum=14)
-
-        self.time_step_spec = ts.time_step_spec(
-            observation_spec=self.observation_spec, reward_spec=self.reward_spec
-        )
-
-
-# Generators
-class DQNGenerator(ProcgenBufferGenerator):
-    """this class generates a buffer using a DQN agent"""
-
-    def __init__(self, tf_env, num_training_steps: int = 10000) -> None:
-        super().__init__(tf_env)
-
-        self.num_train_steps = num_training_steps
-
-        q_net = q_network.QNetwork(
-            self.observation_spec,
-            self.action_spec,
-            fc_layer_params=(100,),
-        )
-
-        self.train_step_counter = tf.Variable(0)
-
-        self.agent = dqn_agent.DqnAgent(
-            self.time_step_spec,
-            self.action_spec,
-            q_network=q_net,
-            optimizer=tf.compat.v1.train.AdamOptimizer(0.001),
-            train_step_counter=self.train_step_counter,
-        )
-
-        self.agent.initialize()
